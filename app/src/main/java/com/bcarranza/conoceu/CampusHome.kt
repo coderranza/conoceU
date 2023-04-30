70124b205a1b255fdd87a9569587e900c7abc21b
coderranza <bryancarranza.works@gmail.com>
2023-04-29 23:19:53 -0600

    All necessary activities has been created just like a template.

---
+++ app/src/main/java/com/bcarranza/conoceu/CampusHome.kt
@@ -0,0 +1,11 @@
+package com.bcarranza.conoceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CampusHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_campus_home)
+    }
+}
---
+++ app/src/main/java/com/bcarranza/conoceu/CampusMenu.kt
@@ -0,0 +1,11 @@
+package com.bcarranza.conoceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CampusMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_campus_menu)
+    }
+}
